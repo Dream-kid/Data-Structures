785c592ca544ee547c45ed794e922df20752e833
Soarov Chakraborty <sourovchakraborty420@gmail.com>
2020-03-01 09:13:44 +0600

    Update MO’s Algorithm.cpp

--- MO’s Algorithm.cpp
+++ MO’s Algorithm.cpp
@@ -1,137 +1,85 @@
 
 //http://www.lightoj.com/volume_showproblem.php?problem=1188
+#include<bits/stdc++.h>
 using namespace std;
- #define sf scanf
-#define pf printf
-#define pi 2*acos(0.0)
-#define ull unsigned long long
-#define all(v) v.begin(),v.end()
- 
-#define sii(t) scanf("%d",&t)
-#define sll(t) scanf("%lld",&t)
-#define sll(t) scanf("%d",&t)
-#define ssll(a,b) scanf("%d%d",&a,&b)
-#define ssd(a,b) scanf("%lf%lf",&a,&b)
-#define sssll(a,b,c) scanf("%lld%lld%lld",&a,&b,&c)
-#define ssslf(a,b,c) scanf("%lf%lf%lf",&a,&b,&c)
-#define ssssll(a,b,c,d) scanf("%lld%lld%lld%lld",&a,&b,&c,&d)
-#define Case(no) printf("Case %d:\n",++no)
-#define nl puts("")
-#define pll(a) printf("%d\n",a)
-#define P(a) printf("%d ",a)
-#define PLL(a) printf("%lld ",a)
- 
- 
-#define ff first
-#define pb push_back
-#define mp make_pair
-#define se second
-#define pb push_back
-#define ST(v) sort(all(v))
-#define gcd(a,b) __gcd(a,b)
-#define lcm(a,b) (a*(b/gcd(a,b)))
-#define max3(a,b,c) max(a,max(b,c))
-#define min3(a,b,c) min(a,min(b,c))
-#define maxall(v) *max_element(all(v))
-#define minall(v) *min_element(all(v))
-#define cover(a,d) memset(a,d,sizeof(a))
-#define output freopen("out.txt","w",stdout)
-#define input freopen("in.txt","r",stdin)
-#define flop(m,n,q)             for(ll i=m;i<=n;i+=q)
-#define optimize ios::sync_with_stdio(0), cin.tie(0)
-////============ CONSTANT ===============////
-#define mx  (100010)
-#define inf 200000
-#define eps 1e-9
-#define mod1  1000000009
-//queen moves
-//int fx[]={0,0,1,-1,1,1,-1,-1};
-//int fy[]={1,-1,0,0,1,-1,1,-1};
- 
-//knight moves
-//int fx[]={-2,-2,-1,-1,1,1,2,2};
-//int fy[]={-1,1,-2,2,-2,2,-1,1};
 typedef int ll;
-//#pragma GCC optimize("O3")
-//#pragma GCC optimize("Ofast,no-stack-protector")
-//#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx")
-//#pragma GCC target("avx,tune=native")
-//float __attribute__((aligned(32)))
- 
- 
-////=====================================////
-///-----SOURAV CHAKRABORTY-----///
-////=====================================////
-ll arr[100010];
-ll frq[100010];
-ll track[100010];
-ll cnt;
-struct mew
+#define inf 1000000010000
+#define mod1 1000000007
+ll track[1000000];
+ll arr[1000000];
+ll ans[1000000];
+bool mark;
+vector<pair<ll,pair<ll,pair<ll,ll> > > >v;
+ll cnt1;
+void add(ll val)
 {
-    ll left,right,track;
-} tree[100010];
-bool comp(mew left,mew right)
-{
-    if(left.left/cnt!=right.left/cnt) return left.left/cnt<right.left/cnt;
-    else return left.right/cnt<right.right/cnt;
+    track[val]++;
+    if(track[val]==1)
+        cnt1++;
 }
-void add(ll idx)
+void reduce(ll val)
 {
-    frq[arr[idx]]++;
-    if(frq[arr[idx]]==1) cnt++;
-}
-void remove(ll idx)
-{
-    frq[arr[idx]]--;
-    if(frq[arr[idx]]==0) cnt--;
+    track[val]--;
+    if(!track[val])
+        cnt1--;
 }
 int main()
 {
-    ll a,t=0;
-    sll(a);
-    while(a--)
+    ll ts;
+    cin>>ts;
+    for(ll t=1; t<=ts; t++)
     {
-        ll c,d;
-        ssll(c,d);
-        cnt=sqrt(c);
-        for(ll i=0; i<c; i++)sll(arr[i]);
-        for(ll i=0; i<d; i++)
+        memset(track,0,sizeof track);
+        ll total,cnt;
+        cin>>total>>cnt;
+        for(ll i=1; i<=total; i++)
+            scanf("%d",&arr[i]);
+        ll temp=sqrt(cnt);
+        for(ll i=1; i<=cnt; i++)
         {
-            ssll(tree[i].left,tree[i].right);
-            tree[i].track=i;
+            ll l,r;
+            scanf("%d %d",&l,&r);
+            v.push_back(make_pair(l/temp,make_pair(r,make_pair(l,i))));
         }
-        sort(tree,tree+d,comp);
-        cnt=0;
-        ll start,end,left_pointer=0,right_pointer=0;
-        for(ll i=0; i<d; i++)
+        sort(v.begin(),v.end());
+        ll left_pointer=0;
+        ll right_pointer=0;
+        cnt1=0;
+        for(ll i=0; i<v.size(); i++)
         {
-            start=tree[i].left-1;
-            end=tree[i].right-1;
-            while(left_pointer>start)
+            ll l=v[i].second.second.first;
+            ll pos=v[i].second.second.second;
+            ll r=v[i].second.first;
+            ll val;
+            while(left_pointer<l)
             {
-                left_pointer--;
-                add(left_pointer);
-            }
-            while(left_pointer<start)
-            {
-                remove(left_pointer);
+                val=arr[left_pointer];
+                reduce(val);
                 left_pointer++;
             }
-            while(right_pointer>end+1)
+            while(right_pointer>r+1)
             {
                 right_pointer--;
-                remove(right_pointer);
+                val=arr[right_pointer];
+                reduce(val);
             }
-            while(right_pointer<=end)
+            while(left_pointer>l)
             {
-                add(right_pointer);
-                right_pointer++;
+                left_pointer--;
+                val=arr[left_pointer];
+                add(val);
             }
-            track[tree[i].track]=cnt;
+            while(right_pointer<=r)
+            {
+                val=arr[right_pointer];
+                right_pointer++;
+                add(val);
+            }
+            ans[pos]=cnt1;
         }
-        Case(t);
-        for(ll i=0; i<d; i++)
-            pll(track[i]);
-        cover(frq,0);
+        printf("Case %d:\n",t);
+        for(ll i=1; i<=cnt; i++)
+            printf("%d\n",ans[i]);
+        v.clear();
     }
 }
