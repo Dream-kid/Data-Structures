c4cb77e1f4e32de3428769b0859c5df85f598392
Sourav Chakraborty <sourovchakraborty420@gmail.com>
2021-02-01 03:02:34 +0600

    Update matrix-exponentiation.cpp

--- matrix-exponentiation.cpp
+++ matrix-exponentiation.cpp
@@ -1,90 +1,75 @@
-//LightOj/1132 - Summing up Powers
-
-#include<bits/stdc++.h>
+//https://lightoj.com/problem/summing-up-powers
+#include <bits/stdc++.h>
 using namespace std;
-typedef unsigned int ll;
-typedef long long ll1;
-#define inf 1000000010000
-ll mat[55][55];
-ll st[55][55];
-ll dup[55][55];
+#define ll unsigned int
+#define ll1 long long
+#define inf 10000000
 ll dp[55][55];
-ll ncr(ll n,ll r)
+ll arr[55][55];
+ll dup[55][55];
+ll save[55][55];
+ll k;
+void copy1()
 {
-    if(n==r || !r)
-        return 1;
-    ll &res=dp[n][r];
-    if(res!=-1)
-        return res;
-    return res=(ncr(n-1,r)+ncr(n-1,r-1));
-}
-ll dim;
-void big_mod(ll1 a)
-{
-    if(a==1)
+    for(ll i=1; i<=k; i++)
     {
-        for(ll i=1; i<=dim; i++)
-        {
-            for(ll j=1; j<=dim; j++)
-            {
-                dup[i][j]=mat[i][j];
-            }
-        }
-        return ;
+        for(ll j=1; j<=k; j++)
+            dup[i][j]=save[i][j];
     }
-    if(a%2==0)
+}
+void big_mod(ll1 n)
+{
+    if(n==1)
     {
-        big_mod(a/2);
-        for(ll i=1; i<=dim; i++)
+        for(ll i=1; i<=k; i++)
         {
-            ll temp=dim;
-            ll j1=1;
-            while(temp--)
+            for(ll j=1; j<=k; j++)
+                dup[i][j]=arr[i][j];
+        }
+        return;
+    }
+    else if(n%2==0)
+    {
+        big_mod(n/2);
+
+        for(ll i=1; i<=k; i++)
+        {
+            for(ll j=1; j<=k; j++)
             {
                 ll ans=0;
-                for(ll j=1; j<=dim; j++)
+                for(ll t=1; t<=k; t++)
                 {
-                    ans+=dup[i][j]*dup[j][j1];
+                    ans+=dup[i][t]*dup[t][j];
                 }
-                st[i][j1]=ans;
-                j1++;
-            }
-        }
-        for(ll i=1; i<=dim; i++)
-        {
-            for(ll j=1; j<=dim; j++)
-            {
-                dup[i][j]=st[i][j];
+                save[i][j]=ans;
             }
         }
     }
     else
     {
-        big_mod(a-1);
-        for(ll i=1; i<=dim; i++)
+        big_mod(n-1);
+        for(ll i=1; i<=k; i++)
         {
-            ll temp=dim;
-            ll j1=1;
-            while(temp--)
+            for(ll j=1; j<=k; j++)
             {
                 ll ans=0;
-                for(ll j=1; j<=dim; j++)
-                {
-                    ans+=dup[i][j]*mat[j][j1];
-                }
-                st[i][j1]=ans;
-                j1++;
-            }
-        }
-        for(ll i=1; i<=dim; i++)
-        {
-            for(ll j=1; j<=dim; j++)
-            {
-                dup[i][j]=st[i][j];
+                for(ll t=1; t<=k; t++)
+                    ans+=arr[i][t]*dup[t][j];
+                save[i][j]=ans;
             }
         }
     }
-    return ;
+    copy1();
+    return;
+}
+ll ncr(ll n,ll r)
+{
+    ll &res=dp[n][r];
+    if(res!=-1)
+        return res;
+    if(n==r||!r)
+        return res=1;
+    return res=ncr(n-1,r)+ncr(n-1,r-1);
 }
 int main()
 {
@@ -93,31 +78,25 @@ int main()
     cin>>ts;
     for(ll t=1; t<=ts; t++)
     {
-        ll1 n,k;
+        ll1 n;
         cin>>n>>k;
-        memset(mat,0,sizeof mat);
-        memset(dup,0,sizeof dup);
-        mat[1][1]=1;
-        for(ll i=0; i<=k; i++)
+        arr[1][1]=1;
+        for(ll i=2; i<=k+2; i++)
+            arr[1][i]=ncr(k,i-2);
+
+        for(ll i=2,t1=k; i<=k+2; i++,t1--)
         {
-            mat[1][i+2]=ncr(k,i);
+            for(ll j=i,t2=0; j<=k+2; j++,t2++)
+                arr[i][j]=ncr(t1,t2);
         }
-        for(ll j=2; j<=k+2; j++)
-        {
-            for(ll i=0; i<=k-j+2; i++)
-            {
-                mat[j][j+i]=ncr(k-j+2,i);
-            }
-        }
-        dim=k+2;
-        dup[1][1]=1;
+        k+=2;
         if(n!=1)
             big_mod(n-1);
+        else
+            dup[1][1]=1;
         ll ans=0;
-        for(ll i=1; i<=k+2; i++)
-        {
+        for(ll i=1; i<=k; i++)
             ans+=dup[1][i];
-        }
         cout<<"Case "<<t<<": "<<ans<<endl;
     }
 }
